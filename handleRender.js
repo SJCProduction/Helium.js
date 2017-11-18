2a90779544eadcaec28f35748f14a959e9d08bd2
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-11-18 10:40:50 -0500

    Adds handleRender function/file

--- handleRender.js
+++ handleRender.js
@@ -1,18 +1,8 @@
-function handleRender (route, context, req){
-    let context = {};
-    let html = ReactDOMServer.renderToString(
-      <StaticRouter location={ req.url } context={ context }>
-        <App />
-      </StaticRouter>
-    );
-    return html;
+function handleRender (Component, context, req){
+  let html = ReactDOMServer.renderToString(
+    <StaticRouter location={ req.url } context={ context }>
+      <Component />
+    </StaticRouter>
+  );
+  return html;
 }
-
-// app.get('*', (req, res) => {
-//     const context = {};
-//     // console.log('url', req.url);
-//     let html = ReactDOMServer.renderToString(
-//       <StaticRouter location={ req.url } context={ context }>
-//         <App />
-//       </StaticRouter>
-//     );
