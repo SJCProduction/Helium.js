e39a60b82b393d84b54dcd6ad15966d84e0e40a7
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-11-17 20:41:21 -0500

    Adds in functionality to react html file and insert div to body with stringified root component in html string

--- routing.js
+++ routing.js
@@ -1,13 +1,15 @@
 
-function reroutingHandler(url, document) {
+function reroutingHandler(res, url, HTMLPath) {
   if (url) {
-    
     res.status = 302;
     res.redirect(url);
     res.end();
-    
   } else {
-    res.write(document);
-    res.end();
+    fs.readFile(HTMLPath, 'utf8', function (err, data) {
+      if (err) throw err;
+      const document = data.replace(/<body>(.*)<\/body>/, `<body><div id="root">${html}</div>$1</body>`);
+      res.write(document);
+      res.end();
+    });
   }
 }
