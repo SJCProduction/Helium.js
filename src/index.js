f8e6533cbd24ac146295e9932254dd45214cd064
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-11-21 15:52:11 -0500

    Refactors a bit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
-const React = require('react');
+'use strict';
 const sjcReactDOMServer = require('react-dom/server');
-const sjcFS = require('fs');
+const fs = require('fs');
 
-
-let inputs = {
-  html: '',
-  component: ''
-};
+let inputs = {};
 
 function init(config) {
-  inputs = config;
+  inputs.html = config.html;
+  inputs.component = config.component;
+  inputs.App = config.App;
 }
 
-
-
-
-
 function render(req, res, next) {
-//optimize App/Static for every call
-const App = require(inputs.component).default;
+  // TODO: optimize App/Static for every call
+  // const App = require(inputs.component).default;
 
-const StaticRouter = require('react-router-dom').StaticRouter;
+  // TODO: temporary fix to 'Critical dependency: the request of a dependency is an expression' warning, which causes 'Cannot find module "."' error in webpack bundle
+  const App = inputs.App;
+
+  const StaticRouter = require('react-router-dom').StaticRouter;
 
   const context = {};
   let stringComponent = sjcReactDOMServer.renderToString(
@@ -32,18 +29,17 @@ const StaticRouter = require('react-router-dom').StaticRouter;
     res.status = 302;
     res.redirect(context.url);
   } else {
-    sjcFS.readFile(inputs.html, 'utf8', function (err, data) {
+    fs.readFile(inputs.html, 'utf8', function (err, data) {
       if (err) throw err;
       const document = data.replace(/<body>(.*)<\/body>/, `<body><div id="root">${stringComponent}</div>$1</body>`);
       res.write(document);
       res.end();
     });
   }
-  
 };
 
 function userData() {
-  const ready = sjcFS.readFileSync('./userInput.json', 'utf8');
+  const ready = fs.readFileSync('./userInput.json', 'utf8');
   return JSON.parse(ready);
 };
 
