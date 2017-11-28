1b33df080064c576d6820844c3ff9481bfef4220
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-11-28 14:40:11 -0500

    Remove logging statement for server running on port etc

--- cli.js
+++ cli.js
@@ -20,20 +20,19 @@ const getUserFiles = () => {
 
     //Update Package.json
     fs.readFile('package.json', 'utf8', (err, result) => {
-      if(err) throw err;
+      if (err) throw err;
       const newPjFile = Object.assign({}, JSON.parse(result));
       newPjFile.scripts[userRes.script] = `babel-node ${SSRname}`;
 
       fs.writeFileSync('package.json', JSON.stringify(newPjFile, null, 2), (err) => {
         if (err) throw err;
-      })
+      });
 
       fs.writeFileSync(`${SSRname}`, getServerScript(userRes), (err) => {
         if (err) throw err;
       });
       shell.exec(`npm run ${userRes.script}`);
     });
-    console.log('Serving now running on localhost:3333');
   });
 };
 
