7373e88e8cc9d8770aa7622eb5cf5e7291f8c1de
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-11-30 13:52:40 -0500

    Adds getAppPath that parses user's index js file to get the root component (App) path

--- parser.js
+++ parser.js
@@ -1,5 +1,20 @@
 const fs = require('fs');
 
+const getAppPath = (path) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    const start = data.indexOf('hydrate');
+    const end = data.indexOf(',', start);
+    const inside = data.slice(start + 8, end);
+    const components = inside.match(/<(.*?)>/gi);
+    const ind = Math.floor(components.length / 2);
+    const name = components[ind].replace(/</, '').replace(/\//, '').replace(/>/, '');
+    const regEx = new RegExp(`import(.*?)${name}(.*?)'(.*?)'`);
+    const filePath = data.match(regEx)[3];
+    return filePath;
+  });
+};
+
+
 const injectReduxDep = (path) => {
   const data = fs.readFileSync(path, 'utf8');
 
@@ -17,6 +32,7 @@ const replRender = (path) => {
 };
 
 module.exports = {
+  getAppPath,
   injectReduxDep,
   replRender,
 };
