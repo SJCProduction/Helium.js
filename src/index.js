2b622fd3555c168b81adfcf2fa656c7845431cbf
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-12-01 11:03:06 -0500

    Import serverside and clientside files and export all functionality in one object so that it is available to the user

--- src/index.js
+++ src/index.js
@@ -1,49 +1,70 @@
-'use strict';
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
-const { StaticRouter } = require('react-router-dom');
-const { createStore } = require('redux');
-const { Provider } = require('react-redux');
+const { hydrate, getStore } = require('./clientSide.js');
+const { init, serve } = require('./serverSide.js');
 
-const fs = require('fs');
-
-const inputs = {};
-
-const init = (config) => {
-  inputs.html = config.html;
-  inputs.App = config.App;
-  inputs.id = config.id;
-  inputs.reducer = config.reducer;
-};
-
-const render = (req, res) => {
-  const { App, reducer } = inputs;
-
-  const context = {};
-  const store = createStore(reducer);
-  const preloadedState = store.getState();
-  const stringComponent = ReactDOMServer.renderToString(
-    <Provider store={store}>
-      <StaticRouter location={req.url} context={context}>
-        <App />
-      </StaticRouter>
-    </Provider>
-  );
-  if (context.url) {
-    res.status = 302;
-    res.redirect(context.url);
-  } else {
-    fs.readFile(inputs.html, 'utf8', (err, data) => {
-      if (err) throw err;
-      const regEx = new RegExp(`<div id="${inputs.id}"><\/div>`, 'gi');
-      const document = data.replace(regEx, `<div id="${inputs.id}">${stringComponent}</div><script>window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}</script>`);
-      res.write(document);
-      res.end();
-    });
-  }
-};
+console.log("hyd>>>>>>..", hydrate);
 
 module.exports = {
+  hydrate,
+  getStore,
   init,
-  render,
+  serve,
 };
+
+// let preloadedState = {};
+
+// const getStore = () => {
+//   console.log(preloadedState);
+//   console.log(config.reducer);
+//   createStore(config.reducer, preloadedState);
+// };
+
+// const init = (inputs) => {
+//   console.log('inside init >>>>>>>>>>>>>>>');
+//   config.html = inputs.html;
+//   config.App = inputs.App;
+//   config.reducer = inputs.reducer;
+
+//   config.renderStore = createStore(config.reducer);
+//   preloadedState = config.renderStore.getState();
+// };
+
+// const serve = (req, res) => {
+//   const fs = require('fs');
+//   console.log('inside serve >>>>>>>>>>>>>>>');
+//   console.log(config);
+//   const { App, renderStore } = config;
+  
+//   const context = {};
+//   const stringComponent = ReactDOMServer.renderToString(
+//     <Provider store={renderStore}>
+//       <StaticRouter location={req.url} context={context}>
+//         <App />
+//       </StaticRouter>
+//     </Provider>
+//   );
+//   if (context.url) {
+//     res.status = 302;
+//     res.redirect(context.url);
+//   } else {
+//     fs.readFile(config.html, 'utf8', (err, data) => {
+//       if (err) throw err;
+//       const regEx = new RegExp(`<div id="${process.env.ID}"><\/div>`, 'gi');
+//       const document = data.replace(regEx, `<div id="${process.env.ID}">${stringComponent}</div>`);
+//       res.write(document);
+//       res.end();
+//     });
+//   }
+// };
+
+// const hydrate = (e, inputId) => {
+//   // process.env.ID = inputId;
+//   console.log("iddddddd", inputId);
+//   ReactDOM.hydrate(e, document.getElementById(inputId));
+// };
+
+// export default hydrate;
+// exports = {
+//   getStore,
+//   init,
+//   serve,
+// };
