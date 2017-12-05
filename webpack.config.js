a7f2a77e4fc5a0bd902b5585c74b441c59ea984e
Chris <cli53@binghamton.edu>
2017-12-05 13:46:39 -0500

    pull dev complete

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
+    publicPath: '/assets/',
     libraryTarget: 'commonjs2',
   },
   module: {
