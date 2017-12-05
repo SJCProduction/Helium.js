bef056c6bd10b128076c844a913ecb44e431807a
Chris <cli53@binghamton.edu>
2017-12-05 16:14:01 -0500

    enhanced build/rebuild time by cheap-module-source-map

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
@@ -7,22 +8,29 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
-    publicPath: '/assets/',
     libraryTarget: 'commonjs2',
-    pathInfo: true,
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
+        exclude: /node_modules/,
       },
     ],
   },
   externals: { React: 'commonjs react' },
+  devtool: 'cheap-module-source-map',
   plugins: [
     new webpack.ProvidePlugin({
       React: 'react',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new UglifyJsPlugin({
+      test: /\.jsx?$/,
+      sourceMap: true,
     }),
   ],
 };
