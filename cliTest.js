de2bcc69e8a82ef3c119d72a07789297e5bf9a9d
Chris <cli53@binghamton.edu>
2017-12-08 16:55:18 -0500

    refactored testPerf

--- cliTest.js
+++ cliTest.js
@@ -4,10 +4,9 @@ const { test } = require('./lib/ques');
 const testPerf = async () => {
   try {
     const user = await inquirer.prompt(test);
-    console.log(user);
-  }
-  catch(error) {
-    console.log(error);
+    return user;
+  } catch (error) {
+    throw error;
   }
 };
 
