140887dd69ae3dc3074ddbfd4b9fb47989d44123
Chris <cli53@binghamton.edu>
2017-12-09 16:23:42 -0500

    attempting to retrieve user results

--- cliTest.js
+++ cliTest.js
@@ -6,7 +6,11 @@ const shell = require('shelljs');
 const testPerf = async () => {
   try {
     const user = await inquirer.prompt(test);
-    if (user) shell.exec('node ./node_modules/helium.js/lib/testPerf.js');
+    if (user) {
+      let results = shell.exec('node ./node_modules/helium.js/lib/testPerf.js',{silence:true}).stdout;
+      console.log(results)
+    }
+    
   } catch (error) {
     throw error;
   }
