aa348176a01ff30e36dd996d3a751c126b67281c
Chris <cli53@binghamton.edu>
2017-12-09 21:03:07 -0500

    spinner for loading data implemented

--- cliTest.js
+++ cliTest.js
@@ -3,38 +3,32 @@ const inquirer = require('inquirer');
 const { test } = require('./lib/ques');
 const shell = require('shelljs');
 const testPromise = require('./lib/testPerf');
-console.log('DEFINITION', testPromise);
+const CLI = require ('clui');
+const Spinner = CLI.Spinner
 
 const testPerf = async () => {
   try {
     const user = await inquirer.prompt(test);
     if (user) {
-      // let results = shell.exec('node ./node_modules/helium.js/lib/testPerf.js',{silence:true}).stdout;
+      // let results = await testPromise()
+      const status = new Spinner('Retrieving Performance data...');
+      status.start();
+      const data = testPromise().then(results => {
+        console.log(results.PerformanceGetMetrics)
+        status.stop();
+      })
       // console.log(results)
-
-      // let results2 = testPromise().then(res => console.log(res));
-
-
-      let results3 = await testPromise()
-
-      console.log('RESULTS 3', results3)
-
-
-      // let results2 = shell.exec('node ./node_modules/helium.js/lib/testPerf.js', {async:true});
-      // results2.stdout.on('data', data => {
-      //   console.log('THIS IS OUTSIDE TEST',data);
-      // });
-
-      // shell.exec('node ./node_modules/helium.js/lib/testPerf.js', (code, stdout, stderr) => {
-      //   console.log('Exit code:', code);
-      //   console.log('Program output:', stdout);
-      //   console.log('Program stderr:', stderr);
-      // });
     }
-    
+
   } catch (error) {
     throw error;
   }
 };
-
 testPerf();
+
+
+
+     // let results2 = shell.exec('node ./node_modules/helium.js/lib/testPerf.js', {async:true});
+      // results2.stdout.on('data', data => {
+      //   console.log('THIS IS OUTSIDE TEST',data);
+      // });
