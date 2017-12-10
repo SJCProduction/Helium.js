247766697b3907460cac69eb96c1de4d3bbaf48e
Chris <cli53@binghamton.edu>
2017-12-10 15:43:24 -0500

    checking out to dev

--- cliPrimary.js
+++ cliPrimary.js
@@ -15,24 +15,18 @@ const getUserFiles = async () => {
   try {
     const user = await inquirer.prompt(questions);
     const userRes = Object.assign({}, user);
-    // const userRes = { ...user };
     if ((userRes.component).substring(0, 2) !== './') userRes.component = `./${userRes.component}`;
     if (userRes.reducer && (userRes.reducer).substring(0, 2) !== './') userRes.reducer = `./${userRes.reducer}`;
     const SSRname = `${userRes.servername}.js`;
     fs.readFile('package.json', 'utf8', (error, result) => {
       if (error) throw error;
       const newPjFile = Object.assign({}, JSON.parse(result));
-      // const newPjFile = { ...JSON.parse(result) };
       newPjFile.scripts[userRes.script] = `./node_modules/.bin/webpack && babel-node ${SSRname}`;
       fs.writeFileSync('package.json', JSON.stringify(newPjFile, null, 2));
 
       if (userRes.reducer) fs.writeFileSync(`${SSRname}`, getReduxServerScript(userRes));
       else fs.writeFileSync(`${SSRname}`, getServerScript(userRes));
-      // console.log('And your answers are:', userRes);
       shell.exec(`npm run ${userRes.script}`);
-
-      // function puts(error, stdout, stderr) { console.log(stdout) };
-      // spawn(`npm run ${userRes.script}`, { stdio: 'inherit' })
     });
   } catch (e) {
     throw e;
