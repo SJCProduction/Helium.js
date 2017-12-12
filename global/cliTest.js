e039f84c053cfc75a66815dd51fb03822d7b8b3d
Chris <cli53@binghamton.edu>
2017-12-12 11:20:33 -0500

    userSelection handles logic for perf test located in argvs

--- global/cliTest.js
+++ global/cliTest.js
@@ -2,15 +2,15 @@
 const inquirer = require('inquirer');
 const { test } = require('../lib/ques');
 const testPromise = require('../lib/testPerf');
+const { renderTests } = require('../lib/argvs.js');
 const { argv } = require('yargs');
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const CLI = require('clui');
 
 const { Spinner } = CLI;
 
 const testPerf = async () => {
-  
   try {
     const user = await inquirer.prompt(test);
     if (user) {
@@ -18,55 +18,10 @@ const testPerf = async () => {
       const status = new Spinner('Retrieving Performance data...');
       status.start();
       const results = await testPromise();
-      //  RUN CSR FIRST, SSR Second
-      //  2 objects, SSR/CSR, find diff
-      //  Get Ratio percentage
-      // a global bin command to show results
       status.stop();
       ui.updateBottomBar(JSON.stringify(results, null, 1));
-      let resultsDisplay = {};
-      if (argv.CSR) {
-        resultsDisplay.CSR = results;
-        fs.writeFile(path.join(__dirname, '../stats', 'perfResults.json'), JSON.stringify(resultsDisplay, null, 2), (error) => {
-        console.log(error);
-        });
-      };
-
-      if (argv.SSR) {
-        resultsDisplay.SSR = results;
-        fs.readFile(path.join(__dirname, '../stats', 'perfResults.json'), 'utf8', (error, data) => {
-          const statsObj = Object.assign({}, JSON.parse(data));
-          statsObj.SSR = results;
-          fs.writeFile(path.join(__dirname, '../stats', 'perfResults.json'), JSON.stringify(statsObj, null, 2), (error) => {
-            console.log(error);
-          });
-        });
-      }
-
-      if (argv.diff) {
-        fs.readFile(path.join(__dirname, '../stats', 'perfResults.json'), (error, data) => {
-          const parseStats = JSON.parse(data);
-          const diff = {};
-          diff.DOMLoading = ((parseStats.CSR.webapi.DOMLoading - parseStats.SSR.webapi.DOMLoading) / parseStats.CSR.webapi.DOMLoading) * 100;
-          diff.DOMContentLoaded = ((parseStats.CSR.webapi.DOMContentLoaded - parseStats.SSR.webapi.DOMContentLoaded) / parseStats.CSR.webapi.DOMContentLoaded) * 100;
-          diff.DOMComplete = ((parseStats.CSR.webapi.DOMComplete - parseStats.SSR.webapi.DOMComplete) / parseStats.CSR.webapi.DOMComplete) * 100;
-          console.log(diff);
-        });
-      }
-
-
-      // let CSR = true;
-      // if(CSR) {
-      //   resultsDisplay.CSR = results;
-      //   CSR = false;
-      // } else {
-      //   resultsDisplay.SSR = results;
-      // let diff = resultsDisplay.CSR - resultsDisplay.SSRR;
-      //   let percent = (diff / resultsDisplay.CSR) * 100;
-      //   console.log(`This is the percent:, ${percent}%`)
-      //   CSR = true;
-      // };
-      console.log(resultsDisplay);
+      const userSelection = argv.CSR || argv.SSR || argv.SSR;
+      renderTests(userSelection, results);
     }
   } catch (error) {
     throw error;
