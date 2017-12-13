bdfee72d2726cbcdcbd9cd4fee6990708520f110
shachyjrclear <shachyj.rivas@gmail.com>
2017-12-13 13:23:08 -0500

    Parse stringified object representing state that has been retrieved off on the window object, so that createStore will get an object, not a string --- using JSON.parse

--- react.js
+++ react.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
-const preloadedState = window.__HELIUM_CONFIG__;
+const preloadedState = JSON.parse(window.__HELIUM_CONFIG__);
 delete window.__HELIUM_CONFIG__;
 
 const getStore = (reducer, middleware) => createStore(reducer, preloadedState, middleware);
